23364492e7cddd13509c1815e8e4175a24d6f60f
GiuPassarelli <gb_passarelli@hotmail.com>
2018-09-20 02:35:12 -0300

    Finalizando codigo

--- src/projeto1/Atualiza.java
+++ src/projeto1/Atualiza.java
@@ -15,20 +15,12 @@ public class Atualiza extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest request,
 			HttpServletResponse response) throws ServletException, IOException {
-//		PrintWriter out = response.getWriter();
-//		out.println("<html><body>");
-//		out.println("<form method='post'>");
-//		out.println("Titulo: <input type='text' name='titulo'><br>");
-//		out.println("Texto: <input type='text' name='texto'><br>");
-//		out.println("<input type='submit' value='Submit'>");
-//		out.println("</form>");
-//		out.println("<body><html>");
 		DAO dao = new DAO();
 		Notas nota = new Notas();
 		nota.setId(Integer.valueOf(request.getParameter("id_edit")));
 		nota.setTitulo(request.getParameter("titulo"));
 		nota.setTexto(request.getParameter("texto"));
-		nota.setId_cor(Integer.valueOf(request.getParameter("id_cor")));
+		nota.setId_cor(request.getParameter("id_cor"));
 		dao.altera(nota);
 		dao.close();
 		request.getRequestDispatcher("index.jsp").forward(request, response);
